145ee562e194ae66a58fca5bcda3e37db81e7b47
Nick Az <nick.azurin@gmail.com>
2026-08-16 12:43:52 -0700

    added boilderplate and packages for express-session and redis cache for user authentication sessions

--- routes/users.js
+++ routes/users.js
@@ -10,84 +10,84 @@ const router = express.Router();
 //GET PATHS
 //GET ALL USERS
 router.get("/", (req, res) => {
-  const users = uh.getUsersSafe();
-  res.send(users);
+    const users = uh.getUsersSafe();
+    res.send(users);
 });
 
 //GET SINGLE USER
 router.get("/:id", (req, res) => {
-  const userId = req.params.id;
-  const user = uh.getUserSafe(userId);
-  res.send(user);
+    const userId = req.params.id;
+    const user = uh.getUserSafe(userId);
+    res.send(user);
 });
 
 //POST PATHS
 //ADD USER
 router.put("/", async (req, res) => {
-  var data = req.body;
-  data = await uh.hashNewUserPassword(data);
+    var data = req.body;
+    data = await uh.hashNewUserPassword(data);
 
-  try {
-    fh.addToDataFile(filePath, data);
+    try {
+        fh.addToDataFile(filePath, data);
 
-    res.status(200).json({ message: "Data added successfully!" });
-  } catch (error) {
-    res.status(500).json({ message: "Internal server error" + error });
-  }
+        res.status(200).json({ message: "Data added successfully!" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" + error });
+    }
 });
 
 //UPDATE USER
 router.patch("/update", (req, res) => {
-  const data = req.body;
-  const updatedUsers = uh.updateUsers(data);
+    const data = req.body;
+    const updatedUsers = uh.updateUsers(data);
 
-  res.send(updatedUsers);
+    res.send(updatedUsers);
 });
 
 //UPDATE PASSWORD
 router.patch("/:id/update-password", async (req, res) => {
-  const data = req.body;
-  const userId = req.params.id;
-  const user = uh.getUser(userId);
+    const data = req.body;
+    const userId = req.params.id;
+    const user = uh.getUser(userId);
 
-  if (await uh.userPasswordMatch(user.password, data.currentPassword)) {
-    user.password = await uh.hashPassword(data.newPassword);
+    if (await uh.userPasswordMatch(user.password, data.currentPassword)) {
+        user.password = await uh.hashPassword(data.newPassword);
 
-    try {
-      uh.updateUsers([user]);
-      const updatedUser = uh.getUserSafe(userId);
+        try {
+            uh.updateUsers([user]);
+            const updatedUser = uh.getUserSafe(userId);
 
-      res.send(updatedUser);
-    } catch (error) {
-      res.status(500).json({ message: "Could not update user password" });
+            res.send(updatedUser);
+        } catch (error) {
+            res.status(500).json({ message: "Could not update user password" });
+        }
+    } else {
+        res.status(500).json({ message: "Password Mismatch" });
     }
-  } else {
-    res.status(500).json({ message: "Password Mismatch" });
-  }
 });
 
 //DELETE USERS
 router.delete("/delete", (req, res) => {
-  const idArray = req.body.ids;
+    const idArray = req.body.ids;
 
-  try {
-    uh.deleteUsers(idArray);
-    res.status(200).json({ message: "Data deleted!" });
-  } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
+    try {
+        uh.deleteUsers(idArray);
+        res.status(200).json({ message: "Data deleted!" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 });
 
 //DELETE USER
 router.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  try {
-    uh.deleteUsers([id]);
-    res.status(200).json({ message: "Data Deleted!" });
-  } catch (error) {
-    res.status(500).json({ message: "Ineternal Server Error" });
-  }
+    try {
+        uh.deleteUsers([id]);
+        res.status(200).json({ message: "Data Deleted!" });
+    } catch (error) {
+        res.status(500).json({ message: "Ineternal Server Error" });
+    }
 });
 
 export default router;
