6ca8b5dce7855e7f832843c9eb362150652c8aea
Nicholas Azurin <nicholas.azurin@tidalis.com>
2026-08-25 17:33:08 -0700

    Added system and user error message js files to store error messages, and created errorHelper to accomodate router error handling

---
+++ constants/systemErrors.js
@@ -0,0 +1,7 @@
+export const SYS_ERROR_CODES = {
+    SERVER_ERROR: {
+        status: 500,
+        message: 'Internal Server Error.'
+    },
+    
+}
---
+++ constants/userErrors.js
@@ -0,0 +1,26 @@
+export const USER_ERROR_CODES = {
+    USER_NOT_FOUND: {
+        status: 404,
+        message: 'User Not Found.'
+    },
+    USER_ALREADY_EXISTS: {
+        status: 409,
+        message: 'User Email or Username already exists.',
+    },
+    USER_OBJECT_INVALID: {
+        status: 400,
+        message: 'User Object is invalid.'
+    },
+    USER_UPDATE_FAIL: {
+        status: 500,
+        message: 'Failed to update user.'
+    },
+    USER_PASSWORD_MISMATCH: {
+        status: 500,
+        message: 'Password mismatched'
+    },
+    UNAUTHORIZED: {
+        status: 401,
+        message: 'You do not have permission to access this resource.'
+    }
+}
